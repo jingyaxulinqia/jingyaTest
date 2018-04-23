f250797fe5d7e045f2f0362935e872b1af4b88de
jingyaxulinqia <jingya.xu@linqia.com>
2018-04-23 09:43:15 -0700

    add hello2

---
+++ hello2.py
@@ -0,0 +1,9 @@
+
+
+# **************************************************************************************
+# Here are some unit tests for validating the more complicated logic here.
+#
+# 1. The hadoop-jobs install script should be changed to install linqiapy along side it.
+# 2. lib.py should then be moved into linqiapy
+# 3. These tests can then be moved into the better test framework in linqiapy
+#
